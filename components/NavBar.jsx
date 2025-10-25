c946a9c8c7c2b10171a4ce2bfc41cbd1578ffac9
sttkjoe <breakgames2003@gmail.com>
2025-10-25 20:16:45 +0300

    Fixed images

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,7 +3,7 @@ export default function NavBar()
     return(
         <header>
             <nav>
-                <img src="../public/React-icon.png" alt="React Icon" />
+                <img src="/React-icon.png" alt="React Icon" />
                 <span>ReactFacts</span>
             </nav>
         </header>
