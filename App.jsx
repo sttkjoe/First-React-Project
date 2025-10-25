29717a5425801a780f0362e41c8751545d2662b8
sttkjoe <breakgames2003@gmail.com>
2025-10-25 19:54:06 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Main from "./components/Main"
+import NavBar from "./components/NavBar"
+
+export default function App()
+{
+    return(
+        <>
+            <NavBar />
+            <Main />
+        </>
+    )
+}
---
+++ components/NavBar.jsx
@@ -0,0 +1,11 @@
+export default function NavBar()
+{
+    return(
+        <header>
+            <nav>
+                <img src="../images/React-icon.png" alt="React Icon" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
